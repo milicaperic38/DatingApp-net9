0cef2cb1ce101132efee6c5e6d9291011bed8b94
milicaperic38 <milica38peric15@gmail.com>
2025-07-22 13:22:46 +0200

    Dodajem izvornu kod bazu za API

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+using System.Text;
+using API.Data;
+using API.Extensions;
+using API.Interface;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//jedna instanca po HTTP zahtevu, kreirace se instanca TokenService klase, kada se zavrsi zahtev instanca se unistava
+//znaci u ovom slucaju, kada se korisnik uloguje unistava se instanca servisa, ali to nema veze sa tim koliko token traje
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+//kako da validiramo token
+
+var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+app.UseAuthentication(); // ko si? authentication mora da
+app.UseAuthorization(); // pravo pristupa
+
+app.MapControllers();
+
+app.Run(); //ova komanda izvrsava nasu aplikaciju
