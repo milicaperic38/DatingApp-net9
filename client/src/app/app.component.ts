4d546da1040164cb654386124c35cbff75d94a62
milicaperic38 <milica38peric15@gmail.com>
2025-07-22 15:11:35 +0200

    Add client folder as normal folder

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
+import { AccountService } from '../../_services/account.service';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, HomeComponent]
+})
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+
+}
